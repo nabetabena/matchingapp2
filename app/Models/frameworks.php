ce367c3485bcf326d8d678ba882e56d649569988
watanabe <son_0eg79913325251e@yahoo.co.jp>
2018-11-16 03:16:44 +0900

    コントローラ作成編集ルーティング

--- app/Models/frameworks.php
+++ app/Models/frameworks.php
@@ -3,8 +3,20 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class frameworks extends Model
 {
-    //
+  protected $table = 'frameworks';
+
+  protected $guarded = array('id');
+
+  public $timestamps = false;
+
+  public function getData()
+  {
+    $data = DB::table($this->table)->get();
+
+    return $data;
+  }
 }
